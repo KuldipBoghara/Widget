243af4244831880555c2615a2517824d913dc6f5
Mr-BK <bogharakuldip@gmail.com>
2021-09-01 12:30:40 -0400

    second commit

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,86 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Search = () => {
-  const [term, setTerm] = useState('');
+  const [term, setTerm] = useState('programming');
+  const [debouncedTerm, setDebouncedTerm] = useState(term);
+  const [results, setResults] = useState([]);
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setDebouncedTerm(term);
+    }, 1000);
+
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [term]);
+
+  useEffect(() => {
+    //we used search function because we can't use async-await directly with useEffect
+    const search = async () => {
+      const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+        params: {
+          action: 'query',
+          list: 'search',
+          origin: '*',
+          format: 'json',
+          srsearch: debouncedTerm
+        }
+      });
+
+      setResults(data.query.search);
+    };
+
+    if (debouncedTerm) {
+      search();
+    }
+  }, [debouncedTerm]);
+
+  /*  useEffect(() => {
+    /*2nd Method to use Async, await 
+                                    (async () => {
+                                    await axios.get();
+                                    })( );                                
+    */
+  /* (3rd method) Use of Promises to fetch API response
+    axios.get('sdds').then((response) => {
+        console.log(response);
+    }); 
+
+    if (term && !results.length) {
+      search();
+    } else {
+      const timeoutId = setTimeout(() => {
+        if (term) {
+          search();
+        }
+      }, 900);
+
+      return () => {
+        clearTimeout(timeoutId);
+      };
+    }
+  }, [term, results.length]); */
+
+  const renderedResults = results.map((result) => {
+    return (
+      <div key={result.pageid} className="item">
+        <div className="right floated content">
+          <a
+            className="ui button"
+            href={`https:en.wikipedia.org?curid=${result.pageid}`}
+          >
+            Go
+          </a>
+        </div>
+        <div className="content">
+          <div className="header">{result.title}</div>
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+        </div>
+      </div>
+    );
+  });
 
   return (
     <div>
@@ -17,6 +94,7 @@ const Search = () => {
           />
         </div>
       </div>
+      <div className="ui celled list">{renderedResults}</div>
     </div>
   );
 };
